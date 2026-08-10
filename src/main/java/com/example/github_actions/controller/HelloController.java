cd1b7517ac8b176926f37fd402d44d3ec9c04821
Kanav Rohatgi <kanavr@gmail.com>
2026-08-11 03:46:30 +0530

    first commit

---
+++ src/main/java/com/example/github_actions/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.github_actions.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello from ci/cd pipeline";
+    }
+    
+}
